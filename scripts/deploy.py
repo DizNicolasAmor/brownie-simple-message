0d47dc173db7be5564d032017dca037835e4f373
Nicolas Diz <diz.nicolasamor@gmail.com>
2021-11-02 16:58:38 -0300

    MASTER: update brownie-config.yaml with networks

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,13 @@
-from brownie import SimpleMessage
+from brownie import config, network, SimpleMessage
 from scripts.helpers import get_account
 
 def main():
     account = get_account()
     initial_message = 'Hello world!'
-    simple_message = SimpleMessage.deploy(initial_message, { "from": account }, publish_source=True)
+    must_publish = config["networks"][network.show_active()].get("verify")
+
+    simple_message = SimpleMessage.deploy(
+        initial_message,
+        { "from": account },
+        publish_source=must_publish
+    )
